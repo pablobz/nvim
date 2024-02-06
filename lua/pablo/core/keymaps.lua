a999d726c5db3a231f5c2e2eed7d286b0930ef8b
pablobz <pabloberz@gmail.com>
2024-02-06 18:25:46 +0100

    cambios

--- lua/pablo/core/keymaps.lua
+++ lua/pablo/core/keymaps.lua
@@ -5,3 +5,6 @@ local expr_opts = { noremap = true, expr = true, silent = true }
 -- Visual line wraps
 keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", expr_opts)
 keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", expr_opts)
+
+vim.api.nvim_command ("digraph -- 8212")
+vim.api.nvim_command ("digraph .. 8230")
