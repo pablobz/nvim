4ecd785c2f22c5622c47daade1abaee385dcefd2
Pablo at Omarchy <pabloberz@gmail.com>
2026-09-19 16:27:07 +0200

    parpadeo del cursor

--- init.lua
+++ init.lua
@@ -9,3 +9,10 @@ require("pablo.core.keymaps")
 require("pablo.lazy")
 require("pablo.core.colorscheme")
 require("config.highlights").setup()
+
+-- hace que el cursor parpadee en todos los modos
+vim.opt.guicursor = {
+  'n-v-c:block-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+  'i-ci:ver25-Cursor/lCursor-blinkwait1000-blinkon100-blinkoff100',
+  'r:hor50-Cursor/lCursor-blinkwait100-blinkon100-blinkoff100'
+}
